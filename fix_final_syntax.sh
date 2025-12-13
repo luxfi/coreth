c5a406bc6b45bb9ff2733e85d0fa48c4464734b4
Zach Kelling <z@zeekay.ai>
2025-12-12 17:01:15 -0800

    Remove AI cruft

--- fix_final_syntax.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Fix double closing parentheses
-find . -name "*.go" -type f -exec sed -i 's/common\.Hash(crypto\.Keccak256Hash(\([^)]*\)))/common.Hash(crypto.Keccak256Hash(\1))/g' {} \;
-
-# Fix any remaining double common.Hash wrapping
-find . -name "*.go" -type f -exec sed -i 's/common\.Hash(common\.Hash(/common.Hash(/g' {} \;
-
